1b8fe41f4440060c9c4398cd936d93b6e7d1655a
krina-jani <krinajani123@gmail.com>
2026-08-13 21:08:36 +0530

    feat: initialize student Flutter project and configure Android build settings

--- student/android/app/build.gradle.kts
+++ student/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.rudraboyspg"
-    compileSdk = 37
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
