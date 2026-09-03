9872c9b4049d332dbb3016872e909480ae57c84f
Kran19 <karanmack5356@gmail.com>
2026-09-03 18:51:25 +0530

    feat: create booking submitted screen and configure project dependencies

--- student/lib/features/booking/booking_submitted_screen.dart
+++ student/lib/features/booking/booking_submitted_screen.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import '../registration_submitted/registration_submitted_screen.dart';
 
 class BookingSubmittedScreen extends StatelessWidget {
-  const BookingSubmittedScreen({super.key});
+  final String appReference;
+
+  const BookingSubmittedScreen({
+    super.key,
+    this.appReference = 'REG-2026-0001',
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const RegistrationSubmittedScreen();
+    return RegistrationSubmittedScreen(appReference: appReference);
   }
 }
